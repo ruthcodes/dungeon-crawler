52694a39361dbfa1e0c3aac238ad165495f13d31
Ruth Ashleigh <notruth@notruth.com>
2018-04-21 15:51:59 -0700

    Added constructor and event listeners for keydown

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      width: 40,
+      height: 40,
+      valBoard: [],
+      activeCell: ''
+    };
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown(e){
+
+    if(e.keyCode === 37 || e.keyCode === 65){
+      console.log("pressed left")
+    }
+    if (e.keyCode === 39 || e.keyCode === 68){
+      console.log("pressed right")
+    }
+    if (e.keyCode ===38 || e.keyCode === 87){
+      console.log("pressed up")
+    }
+    if (e.keyCode === 40 || e.keyCode === 83){
+      console.log("pressed down")
+    }
+  }
+
   render() {
     return (
       <div className="App">
-    
+        <Cell />
       </div>
-    );
+    )
   }
+}
+
+function Cell(props) {
+    return (
+      <div className="cell" ></div>
+    )
 }
 
 export default App;
