fe3b2e06ac22585ce9818d21ba56a8b9eac7faeb
Ruth Ashleigh <notruth@notruth.com>
2018-04-26 11:39:17 -0700

    Beginning outline of working map generator. Partial room connections

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      width: 40,
-      height: 40,
+      valBoard: [],
+      width: 60,
+      height: 20,
+      maxRooms: 15,
+      rooms: [],
+
       maxNoTunnels: 100,
       maxTunnelLen: 7,
       currentTunnelLength: 5,
-      valBoard: [],
       activeCell: '',
       features: 45,
-      lastRoom: true
+      lastRoom: true,
+
     };
     this.handleKeyDown = this.handleKeyDown.bind(this);
     this.mapGenerator = this.mapGenerator.bind(this);
@@ -30,27 +34,21 @@ class App extends Component {
     this.placeFeature = this.placeFeature.bind(this);
     this.generateMap = this.generateMap.bind(this);
 
+
+    //attempt 3
+    this.generateMapArray = this.generateMapArray.bind(this);
+    this.addRooms = this.addRooms.bind(this);
+    this.addCorridors = this.addCorridors.bind(this);
+
   }
   componentDidMount() {
     window.addEventListener('keydown', this.handleKeyDown);
-    var gridVal = [];
-    var rowVal = [];
-    for (let i=0; i<this.state.height; i++){
-      for (let x=0; x<this.state.width; x++){
-        rowVal.push(false)
-      }
-      gridVal.push(rowVal);
-      rowVal = [];
-    }
-    this.setState({
-      valBoard: gridVal,
-    })
-    console.log("mounted")
+    this.generateMapArray();
 
   }
 
   componentDidUpdate(){
-    console.log(this.findEdge());
+
   }
 
   componentWillUnmount() {
@@ -58,12 +56,14 @@ class App extends Component {
   }
 
   handleKeyDown(e){
-    this.generateMap();
+
 
     if(e.keyCode === 37 || e.keyCode === 65){
+      this.addRooms();
       console.log("pressed left")
     }
     if (e.keyCode === 39 || e.keyCode === 68){
+      this.addCorridors();
       console.log("pressed right")
     }
     if (e.keyCode ===38 || e.keyCode === 87){
@@ -74,6 +74,153 @@ class App extends Component {
     }
 
   }
+
+//functions for new map attempt 3
+  generateMapArray(){
+    var board = [];
+    var rows = [];
+    for (let i=0; i<this.state.height; i++){
+      for (let x=0; x<this.state.width; x++){
+        rows.push(false)
+      }
+      board.push(rows);
+      rows = [];
+    }
+    this.setState({
+      valBoard: board,
+    })
+  }
+
+  addRooms(){
+    let counter = 0;
+    let maxAttempts = 40;
+
+      while(counter < this.state.maxRooms && maxAttempts > 0){
+        console.log("counter: " + counter + " max: " + this.state.maxRooms)
+        //validLocation is true
+        var validLocation = true;
+        //randomly generate height,width, and locations
+        var height = this.randomNumber(5,8);
+        console.log("height: " + height);
+        var width = this.randomNumber(5,8);
+        console.log("width: " + width);
+        var locationRow = this.randomNumber(2,18);
+        console.log("row: " + locationRow)
+        var locationCol = this.randomNumber(2,58);
+        console.log("col: " + locationCol)
+        //check if height and width + locations is within the bounds of the board
+        //if it's not continue (go back to the top and make new room)
+        if ((width + locationCol > 59) || (height + locationRow > 19)){
+          console.log("trying again, wont fit on board");
+          maxAttempts--;
+          continue;
+        }
+        //col&width x
+        //nested loop, first through location row and then through location col (up to location + size)
+        //plus one ensures 1 between each room
+        //check that there isn't already a room there (true values)
+        //if there is, set validLocation to false, break
+        loop1:
+        for (let i=locationRow -1; i< locationRow + height+1; i++){
+          for (let x=locationCol-1; x< locationCol + width+1; x++){
+            if (this.state.valBoard[i][x] === true){
+              console.log("there's already a room, not putting it here")
+              validLocation = false;
+              maxAttempts--;
+              break loop1;
+            }
+          }
+        }
+
+        let board = this.state.valBoard.slice();
+        //finally, if placeable, nested loop to place room
+        if(validLocation){
+          for (let i=locationRow; i< locationRow + height; i++){
+            for (let x=locationCol; x< locationCol + width; x++){
+              board[i][x] = true
+            }
+          }
+          counter++;
+
+        let rooms = this.state.rooms.slice();
+        rooms.push({
+          index: counter,
+          height: height,
+          width: width,
+          locationRow: locationRow,
+          locationCol: locationCol
+        })
+
+        this.setState({
+          valBoard: board,
+          rooms: rooms,
+        })
+      }
+
+      }
+  }
+
+  addCorridors(){
+    let rooms = this.state.rooms.slice();
+    //sort all the rooms from leftmost to rightmost
+    rooms.sort((a,b)=>(a.locationCol - b.locationCol || a.locationRow - b.locationRow));
+    this.setState({
+      rooms: rooms
+    })
+    console.log(rooms)
+    //loop through rooms
+    let board = this.state.valBoard.slice();
+    rooms.forEach((room, i) =>{
+
+      if(i<rooms.length -1){
+
+        //if next room has higher column (col+width) then it must be to the right
+        if(room.locationCol + room.width < rooms[i+1].locationCol){
+          //choose a random column
+          let from1col = room.locationCol + room.width -1;
+          let from1row = this.randomNumber(room.locationRow, (room.locationRow + room.height))
+
+          let to2col = rooms[i+1].locationCol
+          let to2row = this.randomNumber(rooms[i+1].locationRow, (rooms[i+1].locationRow + rooms[i+1].height))
+
+          if(from1row === to2row){
+            for(let x= from1col; x< to2col; x++){
+              board[from1row][x] = true;
+            }
+          } else {
+            let turn = this.randomNumber(from1col,to2col);
+
+            for(let i=from1col;i<turn;i++){
+              board[from1row][i] = true;
+            }
+            if (from1row < to2row){
+              for(let i=from1row; i<to2row;i++){
+                board[i][turn] = true;
+              }
+            } else {
+              for(let i=from1row;i>to2row;i--){
+                board[i][turn] = true;
+              }
+            }
+            for (let i=turn;i<to2col;i++){
+              board[to2row][i] = true;
+            }
+
+          }
+          //if room is above or below
+        } else {
+
+        }
+      }
+
+    })
+    this.setState({
+      valBoard: board,
+    })
+  }
+    //starts top left
+
+
 
   validMove(row,col,direction){
     console.log("direction: " + direction)
@@ -180,7 +327,7 @@ class App extends Component {
       valBoard:board,
     })
   }
-  
+
   findEdge(){
     let edges = [];
     this.state.valBoard.map((row,i) => {
